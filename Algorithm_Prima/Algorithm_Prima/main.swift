76c752be37c312aa988c1db41505f320a6bcc949
Aleksey Rochev <alexrochevs@gmail.com>
2017-05-23 20:23:44 +0500

    Added new Xcode project

---
+++ Algorithm_Prima/Algorithm_Prima/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm_Prima
+//
+//  Created by Aleksey Rochev on 23/05/2017.
+//  Copyright © 2017 AlekseyRochev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
